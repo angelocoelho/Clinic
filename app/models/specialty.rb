c53a88c368465583e4c0bb529d6a78a52ea30b02
Ângelo Coêlho <angelo-tkd@hotmail.com>
2025-08-19 19:28:51 -0300

    feature: add ransackable_attributes

--- app/models/specialty.rb
+++ app/models/specialty.rb
@@ -2,4 +2,8 @@
 
 class Specialty < ApplicationRecord
   validates :name, presence: true, uniqueness: true
+
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[id name created_at updated_at]
+  end
 end
