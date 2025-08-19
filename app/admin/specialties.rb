3f6997a6a40a7425edfb2cc14c82407a77c312d3
Ângelo Coêlho <angelo-tkd@hotmail.com>
2025-08-19 17:11:50 -0300

    feature: configure Specialty index, filters and form

---
+++ app/admin/specialties.rb
@@ -0,0 +1,20 @@
+ActiveAdmin.register Specialty do
+  permit_params :name
+
+  filter :name
+
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :created_at
+    actions
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :name
+    end
+    f.actions
+  end
+end
