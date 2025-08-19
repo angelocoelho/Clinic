e3b0c2afdec18cbc6d1a2c987cd953da07a39a2d
Ângelo Coêlho <angelo-tkd@hotmail.com>
2025-08-19 17:10:53 -0300

    build: create specialties table with unique index on name

---
+++ db/migrate/20250819200827_create_specialties.rb
@@ -0,0 +1,9 @@
+class CreateSpecialties < ActiveRecord::Migration[7.1]
+  def change
+    create_table :specialties do |t|
+      t.string :name, null: false
+      t.timestamps
+    end
+    add_index :specialties, :name, unique: true
+  end
+end
