16595888d2de34ebfaaeb55701be3b90d8b27aeb
Ângelo Coêlho <angelo-tkd@hotmail.com>
2025-08-19 17:57:20 -0300

    test: add basic test

--- spec/models/specialty_spec.rb
+++ spec/models/specialty_spec.rb
@@ -3,20 +3,20 @@
 require 'rails_helper'
 
 RSpec.describe Specialty, type: :model do
-  describe "validations" do
-    it "is valid with a unique name" do
-      expect(described_class.new(name: "Psicologia")).to be_valid
+  describe 'validations' do
+    it 'is valid with a unique name' do
+      expect(described_class.new(name: 'Psicologia')).to be_valid
     end
 
-    it "requires name" do
+    it 'requires name' do
       s = described_class.new(name: nil)
       expect(s).not_to be_valid
       expect(s.errors[:name]).to be_present
     end
 
-    it "validates uniqueness of name" do
-      described_class.create!(name: "Psicologia")
-      dup = described_class.new(name: "Psicologia")
+    it 'validates uniqueness of name' do
+      described_class.create!(name: 'Psicologia')
+      dup = described_class.new(name: 'Psicologia')
       expect(dup).not_to be_valid
       expect(dup.errors[:name]).to be_present
     end
