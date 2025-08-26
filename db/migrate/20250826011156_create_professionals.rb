eebb382519eb80753e37da1891d2ddf433a198d0
Ângelo Coêlho <angelo-tkd@hotmail.com>
2025-08-25 22:17:21 -0300

    build: create professionals table

---
+++ db/migrate/20250826011156_create_professionals.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class CreateProfessionals < ActiveRecord::Migration[7.1]
+  def change
+    create_table :professional do |t|
+      t.string :name,  null: false
+      t.string :registration_code
+      t.string :email, null: false
+      t.string :phone
+      t.time :workday_start
+      t.time :workday_end
+      t.integer :appointments_count, null: false, default: 0
+      t.timestamps
+    end
+
+    add_index :professional, :email, unique: true
+  end
+end
