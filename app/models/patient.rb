da5877d962ed0e83cd62e41895487b4f07c1a9a9
Ângelo Coêlho <angelo-tkd@hotmail.com>
2025-08-25 23:38:19 -0300

    feature: add ransackable_attributes

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -3,4 +3,8 @@
 class Patient < ApplicationRecord
   validates :name,  presence: true
   validates :email, presence: true, uniqueness: true
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["appointments_count", "birthdate", "created_at", "email", "id", "id_value", "name", "notes", "phone", "updated_at"]
+  end
 end
